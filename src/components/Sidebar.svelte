aa0fc7835b9a1d17ad63d050b06573b0c794bba4
wdkq <110486890+wdkqqq@users.noreply.github.com>
2025-12-02 06:55:37 +0300

    refactor: removed lyrics button, adjusted paddings, and improved structure

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -12,29 +12,27 @@
 </script>
 
 <div
-  class="pt-7 shrink-0 flex flex-col items-start justify-between border-r border-divider min-h-screen bg-background relative transition-all group"
+  class="pt-5 shrink-0 flex flex-col items-start justify-between border-r border-divider min-h-screen bg-background relative transition-all group"
 >
   <div class="flex flex-col w-full px-4 space-y-1">
     <!-- Songs Button -->
     <div
-      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover transition-colors"
+      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover"
     >
       <div class="w-10 h-10 flex items-center justify-center relative">
         <div class="absolute inset-0 rounded-lg bg-transparent scale-125"></div>
-        <Menu class="w-5 h-5 text-secondary relative z-10 transition-colors" />
+        <Menu class="w-5 h-5 text-secondary relative z-10" />
       </div>
     </div>
 
     <!-- Search Button -->
     <div
       on:click={openCommandPalette}
-      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover transition-colors"
+      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover"
     >
       <div class="w-10 h-10 flex items-center justify-center relative">
         <div class="absolute inset-0 rounded-lg bg-transparent scale-125"></div>
-        <Search
-          class="w-5 h-5 text-secondary relative z-10 transition-colors"
-        />
+        <Search class="w-5 h-5 text-secondary relative z-10" />
       </div>
     </div>
 
@@ -44,34 +42,20 @@
     >
       <div class="w-10 h-10 flex items-center justify-center relative">
         <div class="absolute inset-0 rounded-lg bg-[#222222] scale-125"></div>
-        <Music class="w-5 h-5 text-white relative z-10 transition-colors" />
-      </div>
-    </div>
-
-    <!-- Mic Button -->
-    <div
-      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover transition-colors"
-    >
-      <div class="w-10 h-10 flex items-center justify-center relative">
-        <div class="absolute inset-0 rounded-lg bg-transparent scale-125"></div>
-        <MicVocal
-          class="w-5 h-5 text-secondary relative z-10 transition-colors"
-        />
+        <Music class="w-5 h-5 text-white relative z-10" />
       </div>
     </div>
   </div>
 
   <!-- Settings Button -->
-  <div class="flex flex-col w-full px-4 space-y-5 pb-7">
+  <div class="flex flex-col w-full px-4 space-y-5 pb-5">
     <div
       on:click={openSettingsPanel}
-      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover transition-colors"
+      class="active:scale-90 transition group flex items-center space-x-3 p-1 rounded-lg hover:bg-hover"
     >
       <div class="w-10 h-10 flex items-center justify-center relative">
         <div class="absolute inset-0 rounded-lg bg-transparent scale-125"></div>
-        <Settings
-          class="w-5 h-5 text-secondary relative z-10 transition-colors"
-        />
+        <Settings class="w-5 h-5 text-secondary relative z-10" />
       </div>
     </div>
   </div>
